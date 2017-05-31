8ac2f90123ab5f276a63d00765faa77202bd94d0
Ilya <ilya.rassoshenko@res-ltd.com>
2017-05-31 16:19:58 +0100

    Added basic comments to MarkdownParser methods

--- MarkdownParser.cs
+++ MarkdownParser.cs
@@ -8,6 +8,18 @@ namespace MarkdownToHTML
 {
     public static class MarkdownParser
     {
+        //Entry method
+        public static string Parse(string input)
+        {
+            string wordTag = "";
+            string lineTag = "";
+            string parentTag = "";
+            bool parentTagExists = false;
+
+            return ParseSection(wordTag, lineTag, parentTag, input, parentTagExists);
+        }
+
+        //Determines whether the next string in the input matches the currently open word tag
         private static bool CheckIfClosingWordTag(int currentIndex, string input, string openingTag, out int lengthToSkip)
         {
             string currentString = "";
@@ -33,6 +45,7 @@ namespace MarkdownToHTML
             return false;
         }
 
+        //Gets the matching tag from the supplied dictionary for the string provided
         private static bool GetMatchingTagFromDictionary(Dictionary<string, string> dict, string text, TagDictionaryType dictionaryUsed, out string tag, out int numberOfCharactersToSubstitute)
         {
             string stringToSubstitute = "";
@@ -64,55 +77,34 @@ namespace MarkdownToHTML
             return false;
         }
 
+        //Gets the appropriate tag to be inserted based on input and currently open tags
         private static string GetTagToInsert(string input, string lineTag, string parentTag, bool parentTagExists, out NewTagType newTagType, out int numberOfCharactersToSubstitute)
         {
             string tag = null;
-
-            if (!parentTagExists)
-            {
-                if (GetMatchingTagFromDictionary(_dictForParentTags, input, TagDictionaryType.Parent, out tag, out numberOfCharactersToSubstitute))
-                {
-                    newTagType = NewTagType.Parent;
-                    return tag;
-                }
-            }
-
-            if (GetMatchingTagFromDictionary(_dictForLineTags, input, TagDictionaryType.Line, out tag, out numberOfCharactersToSubstitute))
-            {
-                newTagType = NewTagType.Line;
-                return tag;
-            }
-
-            if (string.IsNullOrEmpty(lineTag) && string.IsNullOrEmpty(parentTag) && !parentTagExists)
-            {
-                if (GetMatchingTagFromDictionary(_dictForParagraphTag, input, TagDictionaryType.Paragraph, out tag, out numberOfCharactersToSubstitute))
-                {
-                    newTagType = NewTagType.Parent;
-                    return tag;
-                }
-            }
-
-            if (GetMatchingTagFromDictionary(_dictForWordTags, input, TagDictionaryType.Word, out tag, out numberOfCharactersToSubstitute))
-            {
-                newTagType = NewTagType.Word;
-                return tag;
-            }
-
             newTagType = NewTagType.None;
             numberOfCharactersToSubstitute = 1;
-            return input[0].ToString();
-        }
-      
-        public static string Parse(string input)
-        {
-            string wordTag = "";
-            string lineTag = "";
-            string parentTag = "";
-            bool parentTagExists = false;
 
-            return ParseSection(wordTag, lineTag, parentTag, input, parentTagExists);
-        }
+            if (!parentTagExists)
+                if (GetMatchingTagFromDictionary(_dictForParentTags, input, TagDictionaryType.Parent, out tag, out numberOfCharactersToSubstitute))
+                    newTagType = NewTagType.Parent;
 
+            else if (GetMatchingTagFromDictionary(_dictForLineTags, input, TagDictionaryType.Line, out tag, out numberOfCharactersToSubstitute))
+                newTagType = NewTagType.Line;
+
+            else if (string.IsNullOrEmpty(lineTag) && string.IsNullOrEmpty(parentTag) && !parentTagExists)
+                if (GetMatchingTagFromDictionary(_dictForParagraphTag, input, TagDictionaryType.Paragraph, out tag, out numberOfCharactersToSubstitute))
+                    newTagType = NewTagType.Parent;
+
+            else if (GetMatchingTagFromDictionary(_dictForWordTags, input, TagDictionaryType.Word, out tag, out numberOfCharactersToSubstitute))
+                newTagType = NewTagType.Word;
+
+            if (tag != null)
+                return tag;
+            else
+                return input[0].ToString();
+        }
+        
+        //Adds appropriate closing tag based on the most recently inserted tag
         private static string AddAppropriateClosingTag(string result, NewTagType newTagType, string lineTag, string parentTag)
         {
             if (newTagType == NewTagType.Line)
@@ -123,6 +115,7 @@ namespace MarkdownToHTML
                 return result;
         }
 
+        //Sets up currently open tags for parsing the next section
         private static void SetUpTagsForNextSection(NewTagType newTagType, string tagToInsert, string currentLineTag, out string wordTag, out string lineTag, out string parentTag)
         {
             if (newTagType == NewTagType.Line)
@@ -153,6 +146,7 @@ namespace MarkdownToHTML
             }
         }
 
+        //Main worker method (recursive in conjunction with ParseNextSection())
         private static string ParseSection(string wordTag, string lineTag, string parentTag, string input, bool parentTagExists)
         {
             string result = "";
@@ -161,7 +155,8 @@ namespace MarkdownToHTML
             NewTagType newTagType;
 
             while (currentIndex < input.Length)
-            {
+            {   
+                //if newline - close currently open word and line tags and parse beyond
                 if (input[currentIndex] == '\n')
                 {
                     result = AddClosingTag(result, wordTag);
@@ -179,16 +174,17 @@ namespace MarkdownToHTML
                 }
                 else
                 {
-                    string tagToInsert = GetTagToInsert(input.Substring(currentIndex), lineTag, parentTag, parentTagExists, out newTagType, out lengthToSkip);
-                    result += tagToInsert;
+                    string stringToInsert = GetTagToInsert(input.Substring(currentIndex), lineTag, parentTag, parentTagExists, out newTagType, out lengthToSkip);
+                    result += stringToInsert;
 
+                    //if a new tag has been inserted - close any previous tags if applicable and parse beyond
                     if (newTagType != NewTagType.None)
                     {
                         if (newTagType == NewTagType.Parent)
                             parentTagExists = true;
 
                         AddAppropriateClosingTag(result, newTagType, lineTag, parentTag);
-                        result += ParseNextSection(input, currentIndex + lengthToSkip, parentTagExists, newTagType, tagToInsert, lineTag);
+                        result += ParseNextSection(input, currentIndex + lengthToSkip, parentTagExists, newTagType, stringToInsert, lineTag);
 
                         if (newTagType == NewTagType.Word)
                             ResetTags(wordTag, "", parentTag, out wordTag, out lineTag, out parentTag);
